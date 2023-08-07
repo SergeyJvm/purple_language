00d70328edb331dfc557572353fdc9893149bf77
extreme_0788@bk.ru <Classik0!>
2023-08-07 09:00:18 +0300

    17. Code Style Settings

---
+++ src/main/java/com/language/sdk/PurpleCodeStyleSettings.java
@@ -0,0 +1,12 @@
+package com.language.sdk;
+
+import com.intellij.psi.codeStyle.CodeStyleSettings;
+import com.intellij.psi.codeStyle.CustomCodeStyleSettings;
+
+public class PurpleCodeStyleSettings extends CustomCodeStyleSettings {
+
+    public PurpleCodeStyleSettings(CodeStyleSettings settings) {
+        super("PurpleCodeStyleSettings", settings);
+    }
+
+}
